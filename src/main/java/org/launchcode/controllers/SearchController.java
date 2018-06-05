0c9b990b4c5af9b6d89e9e18b99d9c4a093d7154
Nick Owens <nick_owens@rocketmail.com>
2018-06-04 19:20:34 -0500

    committing final changes to TechJobs/MVC

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,12 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -23,5 +25,32 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices);
+
+/**        ArrayList<HashMap<String, String >> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        Integer results = jobs.size();
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("results", results);
+        return "search";
+*/
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("jobs", jobs);
+            Integer results = jobs.size();
+            model.addAttribute("results", results);
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("jobs", jobs);
+            Integer results = jobs.size();
+            model.addAttribute("results", results);
+            return "search";
+        }
+
+
+    }
 
 }
